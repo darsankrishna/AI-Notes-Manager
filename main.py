71664f3e77dcfe64ba093e83445f52eafab8f6dc
Darsan K <darsankrishna12@gmail.com>
2026-07-21 00:16:10 +0530

    feat: implement view notes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from notes import add_note
+from notes import add_note, view_notes
 
 
 def main():
     while True:
         print("\n====== AI Notes Manager ======")
         print("1. Add Note")
-        print("2. Exit")
-
+        print("2. View Notes")
+        print("3. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
             add_note()
 
         elif choice == "2":
+            view_notes()
+
+        elif choice == "3":
             print("Goodbye!")
             break
 
--- notes.py
+++ notes.py
@@ -9,3 +9,14 @@ def add_note():
         print("Note added successfully!")
     else:
         print("Empty notes are not allowed.")
+
+
+def view_notes():
+    if not notes:
+        print("\nNo notes available.")
+        return
+
+    print("\nYour Notes:")
+
+    for index, note in enumerate(notes, start=1):
+        print(f"{index}. {note}")
